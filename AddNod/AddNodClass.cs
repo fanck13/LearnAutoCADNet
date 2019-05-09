4d0da25790d32bd3b00e2a4b573e33ced0bc6f41
Fan Chengkai <fanckai13@hotmail.com>
2019-05-09 23:07:56 +0800

    add named object dictionary

---
+++ AddNod/AddNodClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.DatabaseServices;
+[assembly:CommandClass(typeof(AddNod.AddNodClass))]
+
+namespace AddNod
+{
+    public class AddNodClass
+    {
+        [CommandMethod("AddNod")]
+        public void AddNod()
+        {
+            Database db = HostApplicationServices.WorkingDatabase;
+            DataTable dataTable = new DataTable();
+            dataTable.TableName = "Table";
+            using (Transaction transaction = db.TransactionManager.StartTransaction())
+            {
+                DBDictionary dictionary = transaction.GetObject(db.NamedObjectsDictionaryId, OpenMode.ForWrite) as DBDictionary;
+                dictionary.SetAt("DataTable", dataTable);
+                transaction.Commit();
+            }
+        }
+    }
+}
