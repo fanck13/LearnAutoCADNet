83f87b6a4b184f3d820b7ca4918aee17a9abd1e6
Fan Chengkai <fanckai13@hotmail.com>
2019-05-15 00:35:01 +0800

    add choose entity collection

---
+++ ChooseEntityCollection/ChooseEntityCollectionClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+
+namespace ChooseEntityCollection
+{
+    public class ChooseEntityCollectionClass
+    {
+        [CommandMethod("ChooseEntityCollection")]
+        public void ChooseEntityCollection()
+        {
+            Database db = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Database;
+            Editor ed = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument.Editor;
+            Entity entity = null;
+            DBObjectCollection EntityCollection = new DBObjectCollection();
+            PromptSelectionResult ents = ed.GetSelection();
+            if (ents.Status == PromptStatus.OK)
+            {
+                using (Transaction transaction = db.TransactionManager.StartTransaction())
+                {
+                    SelectionSet SS = ents.Value;
+                    foreach (ObjectId id in SS.GetObjectIds())
+                    {
+                        entity = (Entity)transaction.GetObject(id, OpenMode.ForWrite, true);
+                        if (entity != null)
+                        {
+                            EntityCollection.Add(entity);
+                        }
+                    }
+                    transaction.Commit();
+                }
+            }
+        }
+    }
+}
